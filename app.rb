06ee33a250bca01e59cb83c431b91ae3f014419b
Cece Zinny <cwzinny@gmail.com>
2018-07-17 13:43:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,4 +6,13 @@ class App < Sinatra::Base
     "It was so awesome to see Karlie yesterday! She put a lot of our pictures and videos on her instagram! I love her so much! She is so nice"
   end
 
+get '/kitty' do 
+  "Hello Kitty!"
+end 
+
+get '/lollipop' do 
+  "I like ice cream!"
+end 
+
+
 end

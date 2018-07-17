31ebaa756142d9fee790031b88b25a24a7bc83bf
Cece Zinny <cwzinny@gmail.com>
2018-07-17 13:36:28 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "It was so awesome to see Karlie yesterday! She put a lot of our pictures and videos on her instagram!"
+    "It was so awesome to see Karlie yesterday! She put a lot of our pictures and videos on her instagram! I love her so much! She is so nice"
   end
 
 end
